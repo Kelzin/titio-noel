b50548eebd19d45b1c702469f823bfd3e2999db2
Kelzin <jade.raquels2020@gmail.com>
2025-12-09 08:21:17 -0300

    Feat: fiz as balas

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+    private float timer;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    public float reloadTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,rotz);
+
+        if(!canFire)
+        {
+            timer += Time.deltaTime;
+            if(timer > reloadTime)
+            {
+                canFire = true;
+                timer = 0;
+
+            }
+        }
+
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
+    }
+}
